4d6ad2e0b58e4d4a8c179a7b2c66a33d9af16e4b
derekmhawkins <derekhawkins.tech@gmail.com>
2018-10-02 12:44:48 -0500

    added review form

--- src/components/tabs/Reviews/ReviewList/index.js
+++ src/components/tabs/Reviews/ReviewList/index.js
@@ -6,6 +6,26 @@ export default class ReviewList extends Component {
     return (
       <div>
         {this.props.gem.reviews.map(review => <ReviewItem review={review} />)}
+        <h5>Submit a review</h5>
+        <form>
+          <div class="form-group">
+            <select class="form-control" name="" id="">
+              <option disabled selected>Rate this product</option>
+              <option>&#9733;</option>
+              <option>&#9733;&#9733;</option>
+              <option>&#9733;&#9733;&#9733;</option>
+              <option>&#9733;&#9733;&#9733;&#9733;</option>
+              <option>&#9733;&#9733;&#9733;&#9733;&#9733;</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" name="" id="" rows="5" placeholder="Write a brief review..."></textarea>
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control" name="" id="" aria-describedby="helpId" placeholder="Email" />
+          </div>
+          <button type="button" class="btn btn-success float-right">Submit Review</button>
+        </form>
       </div>
     )
   }
